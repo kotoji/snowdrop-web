a5bc43f6b1509f143e02c66c8dc4e4b2d8769595
kotoji <koto.chiba.nOne@gmail.com>
2014-12-03 08:02:18 +0900

    gallery?

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,26 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
 
+  def about = Action {
+    Ok(views.html.about())
+  }
+
+  def member = Action {
+    Ok(views.html.member())
+  }
+
+  def gallery = Action {
+    Ok(views.html.gallery())
+  }
+
+  def link = Action {
+    Ok(views.html.link())
+  }
+
+  def blog = Action {
+    Ok(views.html.blog())
+  }
 }
